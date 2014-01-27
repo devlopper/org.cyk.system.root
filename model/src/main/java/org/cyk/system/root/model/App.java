a397c2bec380de99b27d67444e25809b0492f160
CYK Developper Home <kycdev@gmail.com>
2014-01-27 21:07:16 +0000

    Change-Id: I718814aff18cb6c1531e75399ffa2899b7d04e29

---
+++ model/src/main/java/org/cyk/system/root/model/App.java
@@ -0,0 +1,14 @@
+package org.cyk.system.root.model;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ service-api/src/main/java/org/cyk/system/root/service/api/App.java
@@ -0,0 +1,13 @@
+package org.cyk.system.root.service.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
