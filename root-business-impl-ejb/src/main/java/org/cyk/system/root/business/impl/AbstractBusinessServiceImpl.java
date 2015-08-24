80952888ea3e383f52e894b658888da7c424ea9f
Christian Yao Komenan <christian.komenan@dgbf.gouv.ci>
2015-08-24 17:05:21 +0000

    Change-Id: I6f3c7606a46356a3db281c57f9c5a9569f688fad

--- root-business-impl-ejb/src/main/java/org/cyk/system/root/business/impl/AbstractBusinessServiceImpl.java
+++ root-business-impl-ejb/src/main/java/org/cyk/system/root/business/impl/AbstractBusinessServiceImpl.java
@@ -10,27 +10,13 @@ import org.cyk.system.root.business.api.mathematics.NumberBusiness;
 import org.cyk.system.root.business.api.time.TimeBusiness;
 import org.cyk.system.root.business.impl.validation.ExceptionUtils;
 import org.cyk.utility.common.cdi.AbstractBean;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public abstract class AbstractBusinessServiceImpl extends AbstractBean implements BusinessService, Serializable {
 
 	private static final long serialVersionUID = 6437552355933877400L;
 	
-	private transient Logger logger; 
 	@Inject protected TimeBusiness timeBusiness;
 	@Inject protected NumberBusiness numberBusiness;
-
-	@Override
-	protected Logger __logger__() {
-		return getLogger();
-	}
-	
-	public Logger getLogger() {
-		if(logger==null)
-			logger = LoggerFactory.getLogger(getClass());
-		return logger;
-	}
 	
 	protected ExceptionUtils exceptionUtils(){
 	    return ExceptionUtils.getInstance();
