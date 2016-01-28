f113e57d22d37b98e20e2df1cdafefd441e2d52e
Christian Yao Komenan <christian.komenan@dgbf.gouv.ci>
2016-01-28 16:46:05 +0000

    Change-Id: I56adb4bc776bd267f73b7c9f235ab77766204781

---
+++ root-business-impl-ejb/src/main/java/org/cyk/system/root/business/impl/security/RoleDetails.java
@@ -0,0 +1,23 @@
+package org.cyk.system.root.business.impl.security;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang3.StringUtils;
+import org.cyk.system.root.business.impl.AbstractEnumerationDetails;
+import org.cyk.system.root.model.ContentType;
+import org.cyk.system.root.model.security.Role;
+import org.cyk.utility.common.annotation.user.interfaces.Input;
+import org.cyk.utility.common.annotation.user.interfaces.InputText;
+
+public class RoleDetails extends AbstractEnumerationDetails<Role> implements Serializable{
+	
+	private static final long serialVersionUID = -4741435164709063863L;
+	
+	@Input @InputText private String uniformResourceLocators;
+	
+	public RoleDetails(Role role) {
+		super(role);
+		uniformResourceLocators = StringUtils.join(rootBusinessLayer.getRoleUniformResourceLocatorBusiness().findByRole(role)
+				,ContentType.TEXT.getNewLineMarker());
+	}
+}
