847963d8f1458cb54d5d672c23c7bf9120bda4af
Christian Yao Komenan <christian.komenan@dgbf.gouv.ci>
2016-02-01 15:27:00 +0000

    Change-Id: Ia075af4d1aa0bc5726b65d8442812655b988a797

--- root-business-impl-ejb/src/main/java/org/cyk/system/root/business/impl/AbstractEnumerationDetails.java
+++ root-business-impl-ejb/src/main/java/org/cyk/system/root/business/impl/AbstractEnumerationDetails.java
@@ -2,11 +2,15 @@ package org.cyk.system.root.business.impl;
 
 import java.io.Serializable;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import org.cyk.system.root.business.impl.AbstractOutputDetails;
 import org.cyk.system.root.model.AbstractEnumeration;
 import org.cyk.utility.common.annotation.user.interfaces.Input;
 import org.cyk.utility.common.annotation.user.interfaces.InputText;
 
+@Getter @Setter
 public abstract class AbstractEnumerationDetails<ENUMERATION extends AbstractEnumeration> extends AbstractOutputDetails<ENUMERATION> implements Serializable{
 	
 	private static final long serialVersionUID = -4741435164709063863L;
--- root-business-impl-ejb/src/main/java/org/cyk/system/root/business/impl/mathematics/MovementDetails.java
+++ root-business-impl-ejb/src/main/java/org/cyk/system/root/business/impl/mathematics/MovementDetails.java
@@ -2,11 +2,15 @@ package org.cyk.system.root.business.impl.mathematics;
 
 import java.io.Serializable;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import org.cyk.system.root.business.impl.AbstractEnumerationDetails;
 import org.cyk.system.root.model.mathematics.Movement;
 import org.cyk.utility.common.annotation.user.interfaces.Input;
 import org.cyk.utility.common.annotation.user.interfaces.InputText;
 
+@Getter @Setter
 public class MovementDetails extends AbstractEnumerationDetails<Movement> implements Serializable{
 	private static final long serialVersionUID = -4741435164709063863L;
 	
