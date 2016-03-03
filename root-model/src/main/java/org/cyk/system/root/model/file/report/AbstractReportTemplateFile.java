9bbced5deac399ae4fb6833cfcf64254d528744f
Christian Yao Komenan <christian.komenan@dgbf.gouv.ci>
2016-03-03 16:41:06 +0000

    Change-Id: I4f8214b008e5cfb68b86e76c3eb069be6efb1db1

--- root-model/src/main/java/org/cyk/system/root/model/file/report/AbstractReportTemplateFile.java
+++ root-model/src/main/java/org/cyk/system/root/model/file/report/AbstractReportTemplateFile.java
@@ -18,44 +18,44 @@ public abstract class AbstractReportTemplateFile<TEMPLATE> extends AbstractGener
 	protected LabelValueCollection currentLabelValueCollection;
 	protected List<LabelValueCollectionReport> labelValueCollections = new ArrayList<>();
 	
-	protected void labelValue(LabelValueCollection collection,String labelId,String labelValue,String value,Boolean condition){
+	public void labelValue(LabelValueCollection collection,String labelId,String labelValue,String value,Boolean condition){
 		if(!Boolean.TRUE.equals(condition))
 			return;
 		currentLabelValueCollection = collection;
 		currentLabelValueCollection.add(labelId,labelValue, value);
 	}
-	protected void labelValue(LabelValueCollection collection,String labelId,String value,Boolean condition){
+	public void labelValue(LabelValueCollection collection,String labelId,String value,Boolean condition){
 		labelValue(collection, labelId,null, value, condition);
 	}
 	
-	protected void labelValue(String id,String value,Boolean condition){
+	public void labelValue(String id,String value,Boolean condition){
 		labelValue(currentLabelValueCollection, id, value,condition);
 	}
 	
-	protected void labelValue(LabelValueCollection collection,String id,String value){
+	public void labelValue(LabelValueCollection collection,String id,String value){
 		labelValue(collection, id, value,Boolean.TRUE);
 	}
-	protected void labelValue(String id,String value){
+	public void labelValue(String id,String value){
 		labelValue(currentLabelValueCollection,id, value);
 	}
 	
-	protected LabelValue getLabelValue(String id){
+	public LabelValue getLabelValue(String id){
 		return currentLabelValueCollection.getById(id);
 	}
 	
-	protected LabelValueCollectionReport randomLabelValueCollection(Integer count){
+	public LabelValueCollectionReport randomLabelValueCollection(Integer count){
 		LabelValueCollectionReport labelValueCollection = new LabelValueCollectionReport();
 		labelValueCollection.generate();
 		return labelValueCollection;
 	}
 	
-	protected LabelValueCollectionReport randomLabelValueCollection(){
+	public LabelValueCollectionReport randomLabelValueCollection(){
 		return randomLabelValueCollection(5);
 	}
 	
 	/**/
 	
-	private LabelValueCollectionReport getLabelValueCollectionAtIndex(Integer index){
+	public LabelValueCollectionReport getLabelValueCollectionAtIndex(Integer index){
 		return labelValueCollections.get(index.intValue());
 		//return index < labelValueCollections.size() ? labelValueCollections.get(index.intValue()) : null;
 	}
