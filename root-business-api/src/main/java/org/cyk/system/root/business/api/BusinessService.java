e7ff2cc432c82ce37e11291ac7000063cc4c1b19
devlopper <kycdev@gmail.com>
2016-04-05 08:03:21 +0000

    Change-Id: I70eb6a31c8131b5a032efac23950bb3f48d9a352

--- root-business-api/src/main/java/org/cyk/system/root/business/api/BusinessService.java
+++ root-business-api/src/main/java/org/cyk/system/root/business/api/BusinessService.java
@@ -1,6 +1,35 @@
 package org.cyk.system.root.business.api;
 
+import java.io.Serializable;
+import java.util.Collection;
+
+import org.cyk.utility.common.computation.ExecutionProgress;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 public interface BusinessService {
+
+	@Getter @Setter @NoArgsConstructor
+	public static class BusinessServiceCallArguments<OBJECT> implements Serializable{
+		private static final long serialVersionUID = 7151479991050865862L;
+		
+		private Collection<OBJECT> objects;
+		protected ExecutionProgress executionProgress;
+		
+		public BusinessServiceCallArguments(Collection<OBJECT> objects){
+			setObjects(objects);
+		}
+		
+		public void setObjects(Collection<OBJECT> objects){
+			this.objects = objects;
+			if(this.objects != null){
+				if(executionProgress!=null)
+					executionProgress.setTotalAmountOfWorkUsing100AsBase(new Double(this.objects.size()));
+			}
+		}
+		
+	}
 	
 }
