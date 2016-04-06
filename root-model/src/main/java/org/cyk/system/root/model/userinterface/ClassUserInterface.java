ae0ba8562b1333bc2b92db7708f75fb0cbe52c8f
Christian Yao Komenan <christian.komenan@dgbf.gouv.ci>
2016-04-06 17:35:34 +0000

    Change-Id: If6bd933ab61e2c5827961b7c72196a237f57b48e

--- root-model/src/main/java/org/cyk/system/root/model/userinterface/ClassUserInterface.java
+++ root-model/src/main/java/org/cyk/system/root/model/userinterface/ClassUserInterface.java
@@ -15,7 +15,7 @@ public class ClassUserInterface extends AbstractModelElement implements Serializ
 
 	@Getter @Setter private Class<?> clazz,hierarchyHighestAncestorClass;
 	@Getter @Setter private String labelId,label,iconName,iconExtension;
-    @Getter @Setter private String consultViewId,listViewId,editViewId,createManyViewId,selectOneViewId,selectManyViewId;
+    @Getter @Setter private String consultViewId,listViewId,editViewId,createManyViewId,selectOneViewId,selectManyViewId,processManyViewId;
 	
 	@Override
 	public String getUiString() {
