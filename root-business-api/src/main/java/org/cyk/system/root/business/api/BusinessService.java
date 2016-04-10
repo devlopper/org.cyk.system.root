465bc1ae209b53f6d72e6914cc0cb1266c2c210f
devlopper <kycdev@gmail.com>
2016-04-10 19:06:52 +0000

    Change-Id: I9d62e504c06ef8d77a96a454c1cb2ec407c1b3cd

--- root-business-api/src/main/java/org/cyk/system/root/business/api/BusinessService.java
+++ root-business-api/src/main/java/org/cyk/system/root/business/api/BusinessService.java
@@ -2,6 +2,8 @@ package org.cyk.system.root.business.api;
 
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import org.cyk.utility.common.computation.ExecutionProgress;
 
@@ -16,6 +18,7 @@ public interface BusinessService {
 		private static final long serialVersionUID = 7151479991050865862L;
 		
 		private Collection<OBJECT> objects;
+		private Set<String> previousServiceIdentifiers;
 		protected ExecutionProgress executionProgress;
 		
 		public BusinessServiceCallArguments(Collection<OBJECT> objects){
@@ -30,6 +33,22 @@ public interface BusinessService {
 					executionProgress.clear();
 				}
 			}
+		}
+		
+		public Set<String> getPreviousServiceIdentifiers(){
+			if(previousServiceIdentifiers==null)
+				previousServiceIdentifiers = new LinkedHashSet<>();
+			return previousServiceIdentifiers;
+		}
+		
+		public void addPreviousServiceIdentifiers(String...identifiers){
+			if(identifiers!=null)
+				for(String identifier : identifiers)
+					getPreviousServiceIdentifiers().add(identifier);
+		}
+		
+		public Boolean containPreviousServiceIdentifier(String identifier){
+			return getPreviousServiceIdentifiers().contains(identifier);
 		}
 		
 	}
