51159c5c28d7c4597776dd649fc7878d94205d19
devlopper <kycdev@gmail.com>
2016-04-12 22:34:38 +0000

    Change-Id: Iab56939f0a0b9cad20e8ad3349ac66e6bcafbc0a

--- root-business-api/src/main/java/org/cyk/system/root/business/api/Crud.java
+++ root-business-api/src/main/java/org/cyk/system/root/business/api/Crud.java
@@ -1,5 +1,12 @@
 package org.cyk.system.root.business.api;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 public enum Crud {
     CREATE,READ,UPDATE,DELETE
+    ;
+	
+	public static Boolean isCreateOrUpdate(Crud crud){
+		return ArrayUtils.contains(new Crud[]{Crud.CREATE, Crud.UPDATE},crud);
+	}
 }
