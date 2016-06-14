c850c5d6aed81130a3da4145b5ac1f53c7edfb29
Christian Yao Komenan <christian.komenan@dgbf.gouv.ci>
2016-06-14 16:32:59 +0000

    Change-Id: I96daea7a87c0193bf301af882b5bcd2e872a2052

--- root-business-api/src/main/java/org/cyk/system/root/business/api/mathematics/machine/FiniteStateMachineAlphabetBusiness.java
+++ root-business-api/src/main/java/org/cyk/system/root/business/api/mathematics/machine/FiniteStateMachineAlphabetBusiness.java
@@ -1,8 +1,13 @@
 package org.cyk.system.root.business.api.mathematics.machine;
 
+import java.util.Collection;
+
 import org.cyk.system.root.business.api.AbstractEnumerationBusiness;
+import org.cyk.system.root.model.mathematics.machine.FiniteStateMachine;
 import org.cyk.system.root.model.mathematics.machine.FiniteStateMachineAlphabet;
 
 public interface FiniteStateMachineAlphabetBusiness extends AbstractEnumerationBusiness<FiniteStateMachineAlphabet> {
 
+	Collection<FiniteStateMachineAlphabet> findByMachine(FiniteStateMachine machine);
+	
 }
--- root-business-impl-ejb/src/main/java/org/cyk/system/root/business/impl/mathematics/machine/FiniteStateMachineAlphabetBusinessImpl.java
+++ root-business-impl-ejb/src/main/java/org/cyk/system/root/business/impl/mathematics/machine/FiniteStateMachineAlphabetBusinessImpl.java
@@ -1,11 +1,15 @@
 package org.cyk.system.root.business.impl.mathematics.machine;
 
 import java.io.Serializable;
+import java.util.Collection;
 
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 
 import org.cyk.system.root.business.api.mathematics.machine.FiniteStateMachineAlphabetBusiness;
 import org.cyk.system.root.business.impl.AbstractEnumerationBusinessImpl;
+import org.cyk.system.root.model.mathematics.machine.FiniteStateMachine;
 import org.cyk.system.root.model.mathematics.machine.FiniteStateMachineAlphabet;
 import org.cyk.system.root.persistence.api.mathematics.machine.FiniteStateMachineAlphabetDao;
 
@@ -16,6 +20,11 @@ public class FiniteStateMachineAlphabetBusinessImpl extends AbstractEnumerationB
 	@Inject
 	public FiniteStateMachineAlphabetBusinessImpl(FiniteStateMachineAlphabetDao dao) {
 		super(dao); 
+	}
+
+	@Override @TransactionAttribute(TransactionAttributeType.NEVER)
+	public Collection<FiniteStateMachineAlphabet> findByMachine(FiniteStateMachine machine) {
+		return dao.readByMachine(machine);
 	}
 	
 	
