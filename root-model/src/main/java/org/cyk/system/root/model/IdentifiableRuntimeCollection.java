2312184d4905f552833bc73e2bde1d8b7274353b
Christian Yao Komenan <kycdev@gmail.com>
2017-05-31 17:06:40 +0000

    Change-Id: Id9b08a1e0d55c30c255920342864be354abb9417

--- root-model/src/main/java/org/cyk/system/root/model/IdentifiableRuntimeCollection.java
+++ root-model/src/main/java/org/cyk/system/root/model/IdentifiableRuntimeCollection.java
@@ -2,21 +2,25 @@ package org.cyk.system.root.model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.LinkedHashSet;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
-@Getter @Setter
+@Getter @Setter @Accessors(chain=true)
 public class IdentifiableRuntimeCollection<T> implements Serializable {
 
 	private static final long serialVersionUID = -130189077130420874L;
 
 	private Collection<T> collection;
 	private Boolean synchonizationEnabled;
+	private Collection<String> fieldNames;
 	
 	public IdentifiableRuntimeCollection<T> addOne(T item){
 		getCollection().add(item);
@@ -32,6 +36,26 @@ public class IdentifiableRuntimeCollection<T> implements Serializable {
 		if(collection==null)
 			collection=new ArrayList<>();
 		return collection;
+	}
+	
+	public Collection<String> getFieldNames(){
+		if(fieldNames==null)
+			fieldNames=new LinkedHashSet<>();
+		return fieldNames;
+	}
+	
+	public IdentifiableRuntimeCollection<T> addOneFieldName(String fieldName){
+		getFieldNames().add(fieldName);
+		return this;
+	}
+	
+	public IdentifiableRuntimeCollection<T> addManyFieldName(Collection<String> fieldNames){
+		getFieldNames().addAll(fieldNames);
+		return this;
+	}
+	
+	public IdentifiableRuntimeCollection<T> addManyFieldName(String...fieldNames){
+		return addManyFieldName(Arrays.asList(fieldNames));
 	}
 	
 	public Boolean isSynchonizationEnabled(){
