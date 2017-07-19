1e50e6ae20cb61b9b3ae6f51175aaed9b206baec
Christian Yao Komenan <kycdev@gmail.com>
2017-07-19 15:01:42 +0000

    Change-Id: I5b667640995fd86bf99924837475948b5bc9eac1

---
+++ root-persistence-impl-jpa/src/main/resources/queries/geography/location_select_linkedtoperson_birthlocation.sql
@@ -0,0 +1,8 @@
+SELECT contact.GLOBALIDENTIFIER_IDENTIFIER AS 'Global identifier', IFNULL(partygid.CODE,'') AS 'Person' , IFNULL(phonenumber.NUMBER,'') AS 'Number'
+FROM phonenumber
+
+INNER JOIN contact ON contact.IDENTIFIER = phonenumber.IDENTIFIER
+INNER JOIN contactcollection ON contactcollection.IDENTIFIER = contact.COLLECTION_IDENTIFIER
+INNER JOIN party ON party.CONTACTCOLLECTION_IDENTIFIER = contactcollection.IDENTIFIER
+INNER JOIN globalidentifier AS partygid ON partygid.IDENTIFIER = party.GLOBALIDENTIFIER_IDENTIFIER
+INNER JOIN person ON person.IDENTIFIER = party.IDENTIFIER
