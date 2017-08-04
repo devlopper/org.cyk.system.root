ccae2cc960368d9cf76fac0525b0177dd5f1652b
Christian Yao Komenan <kycdev@gmail.com>
2017-08-04 15:55:26 +0000

    Change-Id: I3338fbc5444d10c23c8d32432ab0622dfebf0dab

--- root-persistence-impl-jpa/src/main/java/org/cyk/system/root/persistence/impl/time/ScheduleItemDaoImpl.java
+++ root-persistence-impl-jpa/src/main/java/org/cyk/system/root/persistence/impl/time/ScheduleItemDaoImpl.java
@@ -9,7 +9,8 @@ import org.cyk.system.root.model.time.Schedule;
 import org.cyk.system.root.model.time.ScheduleItem;
 import org.cyk.system.root.persistence.api.time.ScheduleItemDao;
 import org.cyk.system.root.persistence.impl.AbstractCollectionItemDaoImpl;
-import org.cyk.utility.common.helper.StructureQueryLanguageHelper;
+import org.cyk.utility.common.helper.StructuredQueryLanguageHelper;
+import org.cyk.utility.common.helper.StructuredQueryLanguageHelper.Where.Between;
 import org.cyk.utility.common.helper.TimeHelper;
 
 public class ScheduleItemDaoImpl extends AbstractCollectionItemDaoImpl<ScheduleItem,Schedule> implements ScheduleItemDao,Serializable {
@@ -21,18 +22,32 @@ public class ScheduleItemDaoImpl extends AbstractCollectionItemDaoImpl<ScheduleI
 	@Override
 	protected void namedQueriesInitialisation() {
 		super.namedQueriesInitialisation();
-		StructureQueryLanguageHelper structureQueryLanguageHelper = StructureQueryLanguageHelper.getInstance();
-		registerNamedQuery(readWhereFromBetween, _select().whereString(structureQueryLanguageHelper.getBetween("r.instantInterval.from.year", ":fromYear", ":toYear")
-				+ " AND "+structureQueryLanguageHelper.getBetween("r.instantInterval.from.monthOfYear", ":fromMonthOfYear", ":toMonthOfYear")
-				+ " AND "+structureQueryLanguageHelper.getBetween("r.instantInterval.from.dayOfMonth", ":fromDayOfMonth", ":toDayOfMonth")));
+		
+		StructuredQueryLanguageHelper.Builder.Adapter.Default.JavaPersistenceQueryLanguage jpql = new StructuredQueryLanguageHelper.Builder.Adapter.Default.JavaPersistenceQueryLanguage();
+		StructuredQueryLanguageHelper.Where.Between between = new StructuredQueryLanguageHelper.Where.Between.Adapter.Default.JavaPersistenceQueryLanguage().setStructuredQueryLanguageBuilder(jpql);
+		
+		String where = "("+between.setProperty(Between.PROPERTY_NAME_SUFFIX, "1").setProperty(Between.PROPERTY_NAME_FIELD_NAME, "r.instantInterval.from.year").execute()
+				+" AND "+between.clear().setProperty(Between.PROPERTY_NAME_SUFFIX, "1").setProperty(Between.PROPERTY_NAME_FIELD_NAME, "r.instantInterval.from.monthOfYear").execute()
+				+" AND "+between.clear().setProperty(Between.PROPERTY_NAME_SUFFIX, "1").setProperty(Between.PROPERTY_NAME_FIELD_NAME, "r.instantInterval.from.dayOfMonth").execute()
+				+") OR ("+between.clear().setProperty(Between.PROPERTY_NAME_SUFFIX, "2").setProperty(Between.PROPERTY_NAME_FIELD_NAME, "r.instantInterval.to.year").execute()
+				+" AND "+between.clear().setProperty(Between.PROPERTY_NAME_SUFFIX, "2").setProperty(Between.PROPERTY_NAME_FIELD_NAME, "r.instantInterval.to.monthOfYear").execute()
+				+" AND "+between.clear().setProperty(Between.PROPERTY_NAME_SUFFIX, "2").setProperty(Between.PROPERTY_NAME_FIELD_NAME, "r.instantInterval.to.dayOfMonth").execute()
+				+")";
+		
+		registerNamedQuery(readWhereFromBetween, jpql.addTupleCollection("ScheduleItem", "r").addWhere(where).execute());
 	}
 	
 	@Override
 	public Collection<ScheduleItem> readWhereFromBetween(Date from, Date to) {
 		return namedQuery(readWhereFromBetween)
-				.parameter("fromYear", TimeHelper.getInstance().getYear(from)).parameter("toYear", TimeHelper.getInstance().getYear(to))
-				.parameter("fromMonthOfYear", TimeHelper.getInstance().getMonthOfYear(from)).parameter("toMonthOfYear", TimeHelper.getInstance().getMonthOfYear(to))
-				.parameter("fromDayOfMonth", TimeHelper.getInstance().getDayOfMonth(from)).parameter("toDayOfMonth", TimeHelper.getInstance().getDayOfMonth(to))
+				.parameter("fromYear1", TimeHelper.getInstance().getYear(from)).parameter("toYear1", TimeHelper.getInstance().getYear(to))
+				.parameter("fromMonthOfYear1", TimeHelper.getInstance().getMonthOfYear(from)).parameter("toMonthOfYear1", TimeHelper.getInstance().getMonthOfYear(to))
+				.parameter("fromDayOfMonth1", TimeHelper.getInstance().getDayOfMonth(from)).parameter("toDayOfMonth1", TimeHelper.getInstance().getDayOfMonth(to))
+				
+				.parameter("fromYear2", TimeHelper.getInstance().getYear(from)).parameter("toYear2", TimeHelper.getInstance().getYear(to))
+				.parameter("fromMonthOfYear2", TimeHelper.getInstance().getMonthOfYear(from)).parameter("toMonthOfYear2", TimeHelper.getInstance().getMonthOfYear(to))
+				.parameter("fromDayOfMonth2", TimeHelper.getInstance().getDayOfMonth(from)).parameter("toDayOfMonth2", TimeHelper.getInstance().getDayOfMonth(to))
+				
 				.resultMany();
 	}
 
