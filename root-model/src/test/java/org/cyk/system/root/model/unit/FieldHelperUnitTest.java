5c40c14ddd2205fdc4275a85da533c87b8fcf401
Christian Yao Komenan <kycdev@gmail.com>
2017-08-18 07:51:23 +0000

    Change-Id: Idd84da25339a3c7760fdade86f16b4445b310380

---
+++ root-model/src/test/java/org/cyk/system/root/model/unit/FieldHelperUnitTest.java
@@ -0,0 +1,21 @@
+package org.cyk.system.root.model.unit;
+
+
+import org.cyk.system.root.model.party.person.Person;
+import org.cyk.utility.common.helper.FieldHelper;
+import org.cyk.utility.test.unit.AbstractUnitTest;
+
+public class FieldHelperUnitTest extends AbstractUnitTest {
+
+	private static final long serialVersionUID = 8008545189557409317L;
+
+	@Override
+	protected void _execute_() {
+		super._execute_();
+		Person person = new Person();
+		FieldHelper.getInstance().set(person,(Object)null, Person.FIELD_NATIONALITY);
+	}
+	
+	
+	
+}
