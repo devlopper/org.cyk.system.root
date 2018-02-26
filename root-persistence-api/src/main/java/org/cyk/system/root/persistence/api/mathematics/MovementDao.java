6b4f1aa1bc5e93ee6908e5665c209f01036311a1
Christian Yao Komenan <kycdev@gmail.com>
2018-02-26 10:54:10 +0000

    MovementDao

--- root-persistence-api/src/main/java/org/cyk/system/root/persistence/api/mathematics/MovementDao.java
+++ root-persistence-api/src/main/java/org/cyk/system/root/persistence/api/mathematics/MovementDao.java
@@ -1,7 +1,6 @@
 package org.cyk.system.root.persistence.api.mathematics;
 
 import java.math.BigDecimal;
-import java.util.Collection;
 
 import org.cyk.system.root.model.mathematics.Movement;
 import org.cyk.system.root.model.mathematics.MovementCollection;
@@ -10,5 +9,5 @@ import org.cyk.system.root.persistence.api.AbstractCollectionItemDao;
 public interface MovementDao extends AbstractCollectionItemDao<Movement,MovementCollection> {
 
 	BigDecimal sumValueWhereExistencePeriodFromDateIsLessThan(Movement movement);
-	//Collection<Movement> readByParent(Movement parent);
+
 }
